398abcd927cdfd1764fce51191352f853d3d0f06
Naja von Schmude <naja1987@gmail.com>
2010-12-30 11:31:11 +0100

    update project and buildscript to iOS 4.2

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,6 @@
-# builds a "fat" static library for iPhones and Simulator (for iOS 4)
+# builds a "fat" static library for iPhones and Simulator (for iOS 4.2)
 
 #!/bin/bash
-xcodebuild -sdk iphoneos4.1 "ARCHS=armv6 armv7" clean build
-xcodebuild -sdk iphonesimulator4.1 "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" clean build
+xcodebuild -sdk iphoneos4.2 "ARCHS=armv6 armv7" clean build
+xcodebuild -sdk iphonesimulator4.2 "ARCHS=i386 x86_64" "VALID_ARCHS=i386 x86_64" clean build
 lipo -output build/libnARLib.a -create build/Release-iphoneos/libnARLib.a build/Release-iphonesimulator/libnARLib.a
